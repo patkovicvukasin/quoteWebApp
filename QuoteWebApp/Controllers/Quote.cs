c0e98ab298f4ee7b74c95fe5f8d3710a7bc617bf
Vule <vukasinpatkovic@gmail.com>
2025-01-08 17:57:41 +0100

    initial commit

---
+++ QuoteWebApp/Controllers/Quote.cs
@@ -0,0 +1,14 @@
+﻿namespace QuoteWebApp.Controllers
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Author{ get; set; }
+
+        public Quote()
+        {
+
+        }
+    }
+}
---
+++ QuoteWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QuoteWebApp.Controllers;
+
+namespace QuoteWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<QuoteWebApp.Controllers.Quote> Quote { get; set; } = default!;
+    }
+}
